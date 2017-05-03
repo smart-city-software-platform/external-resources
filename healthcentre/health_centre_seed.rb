00d34354b0596730b5874758a2a141944f80d54d
Lucas Helfstein <lucashelfstein@gmail.com>
2017-05-03 15:54:45 -0300

    A generator for fake healthcare data
    Signed-off-by: Lucas Helfstein <lucashelfstein@gmail.com>
    Signed-off-by: Fernanda de Camargo <fernandacamargo_24@hotmail.com>
    Signed-off-by: Macartur Carvalho <macartur.sc@gmail.com>

--- healthcentre/health_centre_seed.rb
+++ healthcentre/health_centre_seed.rb
@@ -1,76 +1,86 @@
 require 'csv'
+require 'geocoder'
+require 'faker'
 require_relative 'utils'
 
-
-db = DB.new
-
-health_centre_csv_path = File.join(__dir__, "csv/health_centres.csv")
-health_centre_types_csv_path = File.join(__dir__, "csv/health_centres_types.csv")
-specialties_csv_path = File.join(__dir__, "csv/specialties.csv")
-types_csv_path = File.join(__dir__, "csv/type.csv")
-data_csv_path = File.join(__dir__, "csv/data.csv")
-
-puts "LOAD HEALTH_CENTRES: "
-CSV.foreach(health_centre_csv_path, :headers => true) do |row|
-  description = "Health Centre with CNES #{row[0]} NAME #{row[1]} BEDS #{row[2]}"
-  hc = HealthCentre.new cnes: row[0], description: description
-  db.health_centres[hc.cnes] = hc
-  print "."
+def lat_range (min=-23.69, max=-23.44)
+    rand * (max-min) + min
 end
 
-puts "LOAD SPECIALTIES: "
-CSV.foreach(specialties_csv_path, :headers => false) do |row|
-  s = Specialty.new id: row[0], name: row[1]
-  db.specialties[s.id] = s
-  print "."
+def lon_range (min=-46.36, max=-46.84)
+    rand * (max-min) + min
 end
 
-puts "LOAD HEALTH CENTRE TYPE: "
-CSV.foreach(types_csv_path, :headers => false) do |row|
-  t = Type.new(id: row[0], name: row[1])
-  db.types[t.id] = t
-  print "."
-end
+def my_db
 
-puts "ASSOCIATE TYPE WITH HEALTHCENTRE: "
-CSV.foreach(health_centre_types_csv_path, :headers => false) do |row|
-  t = db.types[row[1]]
-  hc = db.health_centres[row[0]]
-  hc.types << t
-  print "."
-end
+  db = DB.new
 
+  health_centre_csv_path = File.join(__dir__, "csv/health_centres.csv")
+  health_centre_types_csv_path = File.join(__dir__, "csv/health_centres_types.csv")
+  specialties_csv_path = File.join(__dir__, "csv/specialties.csv")
+  types_csv_path = File.join(__dir__, "csv/type.csv")
 
-# CONVERT LAT AND LONG TO INTEGER WITHOUT '.'
-
-def convert_geo value
-  # TODO: fix precision remove dots
-  value.to_f
-end
-
-puts "SEND PROCEDURE DATA"
-count = 0
-CSV.foreach(data_csv_path, :headers => true) do |row|
-  break if count == 20
-  specialty_id = row[11].to_i
-  if specialty_id < 10
-   p = Procedure.new(cnes_id: row[6], specialty: db.specialties[specialty_id.to_s].name,
-                     date: Date.parse(row[8]), gender: row[2],
-                     different_district: row[12],
-                     lat: convert_geo(row[0]),
-                     long: convert_geo(row[1]))
-
-   hc = db.health_centres[p.cnes_id]
-
-   hc.update lat: convert_geo(row[4]), lon: convert_geo(row[5])
-   hc.register
-   # THIS CAN BE CHANGED
-   hc.send_data value: p.to_hash, date: p.date
-   print "."
-   count += 1
+  puts "LOAD HEALTH_CENTRES: "
+  CSV.foreach(health_centre_csv_path, :headers => true) do |row|
+    description = "Health Centre with CNES #{row[0]} NAME #{row[1]} BEDS #{row[2]}"
+    hc = HealthCentre.new cnes: row[0], description: description, lat: lat_range , lon: lon_range
+    db.health_centres[hc.cnes] = hc
+    print "."
   end
+
+  puts "LOAD SPECIALTIES: "
+  CSV.foreach(specialties_csv_path, :headers => false) do |row|
+    s = Specialty.new id: row[0], name: row[1]
+    db.specialties[s.id] = s
+    print "."
+  end
+
+  puts "LOAD HEALTH CENTRE TYPE: "
+  CSV.foreach(types_csv_path, :headers => false) do |row|
+    t = Type.new(id: row[0], name: row[1])
+    db.types[t.id] = t
+    print "."
+  end
+
+  puts "ASSOCIATE TYPE WITH HEALTHCENTRE: "
+  CSV.foreach(health_centre_types_csv_path, :headers => false) do |row|
+    t = db.types[row[1]]
+    hc = db.health_centres[row[0]]
+    hc.types << t
+    print "."
+  end
+
+  db
+
 end
 
-db.save
+########################
+# Random Procedures code
 
-# TODO: UPDATE HEALTH_CENTRE SPECIATIES
+
+def send_procedure db
+  lat = lat_range
+  long = lon_range
+  hc = db.health_centres.values.sample
+  p = Procedure.new(cnes_id: hc.cnes, 
+                     specialty: db.specialties.values.sample.name,
+                     date: Faker::Time.between(Date.today-900, Date.today-700), 
+                     gender: ['M','F'].sample,
+                     different_district: Faker::Name.last_name,
+                     lat: lat,
+                     long: long)
+  hc.register
+  hc.send_data value: p.to_hash, date: p.date
+end
+
+def seed n=5
+  puts "SEEDING: "   
+  db = my_db
+  for x in 0..n
+    send_procedure db
+    print '.'
+  end
+  db.save
+end
+
+seed 20 
